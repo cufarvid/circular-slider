51d674ee10f996cf3ca9cb5564b590b787f0b4e5
Vid Čufar <vid@cufar.dev>
2023-08-17 18:39:28 +0200

    feat: clean-up utils
    Signed-off-by: Vid Čufar <vid@cufar.dev>

--- src/util.ts
+++ src/util.ts
@@ -1,29 +1,47 @@
 import { Coordinates, SVGElementAttribute } from './types';
 
+/**
+ * Converts polar coordinates to Cartesian coordinates.
+ *
+ * @param {Coordinates} coordinates The center of the Cartesian coordinate system.
+ * @param {number} radius The radial distance from the center point.
+ * @param {number} angleInDegrees The angle in degrees.
+ *
+ * @returns {Coordinates} The Cartesian coordinates.
+ */
 export function polarToCartesian(
-  centerX: number,
-  centerY: number,
+  coordinates: Coordinates,
   radius: number,
   angleInDegrees: number,
 ): Coordinates {
+  const { x, y } = coordinates;
   const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 
   return {
-    x: centerX + radius * Math.cos(angleInRadians),
-    y: centerY + radius * Math.sin(angleInRadians),
+    x: x + radius * Math.cos(angleInRadians),
+    y: y + radius * Math.sin(angleInRadians),
   };
 }
 
+/**
+ * Creates an SVG path description for an arc.
+ *
+ * @param {Coordinates} coordinates The center of the arc.
+ * @param {number} radius The radius of the arc.
+ * @param {number} startAngle The starting angle in degrees.
+ * @param {number} endAngle The ending angle in degrees.
+ *
+ * @returns {string} The SVG path description.
+ */
 export function describeArc(
   coordinates: Coordinates,
   radius: number,
   startAngle: number,
   endAngle: number,
 ): string {
-  const { x, y } = coordinates;
   const fullCircle = endAngle - startAngle === 360;
-  const start = polarToCartesian(x, y, radius, endAngle - 0.01);
-  const end = polarToCartesian(x, y, radius, startAngle);
+  const start = polarToCartesian(coordinates, radius, endAngle - 0.01);
+  const end = polarToCartesian(coordinates, radius, startAngle);
   const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
 
   // prettier-ignore
@@ -34,6 +52,15 @@ export function describeArc(
   ].join(' ');
 }
 
+/**
+ * Creates an SVG element with specified attributes.
+ *
+ * @template K SVG element tag name.
+ * @param {K} qualifiedName The SVG element tag name.
+ * @param {Object.<SVGElementAttribute, string | number>} attributes The attributes of the element.
+ *
+ * @returns {SVGElementTagNameMap[K]} The created SVG element.
+ */
 export function createElementNS<K extends keyof SVGElementTagNameMap>(
   qualifiedName: K,
   attributes: { [key in SVGElementAttribute]?: string | number },
@@ -50,10 +77,24 @@ export function createElementNS<K extends keyof SVGElementTagNameMap>(
   return element;
 }
 
+/**
+ * Converts radians to degrees.
+ *
+ * @param {number} radians - The angle in radians.
+ *
+ * @returns {number} The angle in degrees.
+ */
 export function radiansToDegrees(radians: number): number {
   return (radians * 180) / Math.PI + 90;
 }
 
+/**
+ * Retrieves client coordinates from mouse or touch events.
+ *
+ * @param {MouseEvent | TouchEvent} event The event containing client coordinates.
+ *
+ * @returns {Coordinates} The client coordinates.
+ */
 export function getClientCoordinates(
   event: MouseEvent | TouchEvent,
 ): Coordinates {
